71178dc830cf2eeece090a187280b0d511011cf2
alanpenze <alan-penze@uiowa.edu>
2019-02-18 16:35:47 -0600

    Added some stuff for use later

--- src/main/java/com/vanpool/vanpool/DAO/DriverDao.java
+++ src/main/java/com/vanpool/vanpool/DAO/DriverDao.java
@@ -1,4 +1,16 @@
 package com.vanpool.vanpool.DAO;
 
+import com.vanpool.vanpool.Entity.Driver;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class DriverDao {
+    private static Map<Integer, Driver> Drivers;
+
+    static {
+        //Drivers = new HashMap<Integer, Driver>(){
+
+        
+    }
 }
