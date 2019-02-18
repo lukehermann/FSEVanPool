f3f9172dc7c099adc77d0b30e451757c527f74a1
lukehermann <luke-hermann@uiowa.edu>
2019-02-18 17:32:14 -0600

    Rider and Van class implemented.

---
+++ src/main/java/com/vanpool/vanpool/Entity/Van.java
@@ -0,0 +1,32 @@
+package com.vanpool.vanpool.Entity;
+
+public class Van {
+
+    // ID, Location
+
+    private int id;
+    private String location;
+
+    public Van(int id, String location){
+
+        this.id = id;
+        this.location = location;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
