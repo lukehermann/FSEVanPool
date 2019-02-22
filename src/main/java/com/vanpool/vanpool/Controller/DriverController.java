c66e7bbefcefda5b018a55eb4d8a10f4fde80ba6
alanpenze <alan-penze@uiowa.edu>
2019-02-22 11:38:22 -0600

    Working on some controller/service/DAO stuff

--- src/main/java/com/vanpool/vanpool/Controller/DriverController.java
+++ src/main/java/com/vanpool/vanpool/Controller/DriverController.java
@@ -1,4 +1,17 @@
 package com.vanpool.vanpool.Controller;
 
+import com.vanpool.vanpool.Entity.Driver;
+import com.vanpool.vanpool.Service.DriverService;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
 public class DriverController {
+    private DriverService driverService;
+
+    public Collection<Driver> getAllDrivers()
+    {
+        return driverService.getAllDrivers();
+    }
 }
