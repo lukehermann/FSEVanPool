36ff5af4dd15083ce796bac98e717d795d7653bb
alanpenze <alan-penze@uiowa.edu>
2019-03-10 16:37:16 -0500

    Created a home page for a passenger where the passenger can input their info, no testing done yet

--- src/main/java/com.springboot.configuration/SecurityConfiguration.java
+++ src/main/java/com.springboot.configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,6 +17,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@ComponentScan
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Autowired
